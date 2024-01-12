e7d1cb728f020c2ba3ab9c5091153e3375863891
vpyasi11 <vpyasi11@gmail.com>
2024-01-12 20:14:12 +0530

    change in routes

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ export default function App() {
     <BrowserRouter>
     <Routes>
       <Route initialRouteName={user ? 'TodoList' : 'Login'}>
-        <Route name="Login" component={LoginScreen} />
-        <Route name="Profile" component={ProfileScreen} />
-        <Route name="TodoList" component={TodoListScreen} />
-        <Route name="AddTask" component={AddTaskScreen} />
-        <Route name="TaskDetail" component={TaskDetailScreen} />
+        <Route path="/Login" element={LoginScreen} />
+        <Route path="/Profile" element={ProfileScreen} />
+        <Route path="/TodoList" element={TodoListScreen} />
+        <Route name="/AddTask" element={AddTaskScreen} />
+        <Route name="/TaskDetail" element={TaskDetailScreen} />
       </Route>
 
     </Routes>
