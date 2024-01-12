45f7b3403b1c81f3c753bda81027e8e694c1ec30
Chandramani Pyasi <118059919+vpyasi11@users.noreply.github.com>
2024-01-12 22:36:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginScreen from './components/LoginScreen';
+import TodoListScreen from './components/TodoListScreen';
+import AddTaskScreen from './components/AddTaskScreen';
+import TaskDetailScreen from './components/TaskDetailScreen';
+import ProfileScreen from './components/ProfileScreen';
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage
+
 export default function App() {
   const [user, setUser] = useState(false);
 
@@ -15,17 +23,22 @@ export default function App() {
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route initialRouteName={user ? 'TodoList' : 'Login'}>
-        <Route path="/Login" element={LoginScreen} />
-        <Route path="/Profile" element={ProfileScreen} />
-        <Route path="/TodoList" element={TodoListScreen} />
-        <Route name="/AddTask" element={AddTaskScreen} />
-        <Route name="/TaskDetail" element={TaskDetailScreen} />
-      </Route>
-
-    </Routes>
+      <Routes>
+        {user ? (
+          <>
+            <Route path="/Profile" element={<ProfileScreen />} />
+            <Route path="/TodoList" element={<TodoListScreen />} />
+            <Route path="/AddTask" element={<AddTaskScreen />} />
+            <Route path="/TaskDetail" element={<TaskDetailScreen />} />
+            <Route path="/*" element={<Navigate to="/TodoList" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/Login" element={<LoginScreen />} />
+            <Route path="/*" element={<Navigate to="/Login" />} />
+          </>
+        )}
+      </Routes>
     </BrowserRouter>
   );
 }
-  
